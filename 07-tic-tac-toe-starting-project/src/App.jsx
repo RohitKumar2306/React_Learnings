f3a97783320b76c7f1a07aec3af40e6daa895a80
Rohit Kumar Chintamani <rohitkumarchintamani@Mac.cable.rcn.com>
2025-08-21 16:16:50 -0500

    Initial UI page for the game

---
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -0,0 +1,20 @@
+import Player from "./Components/Player.jsx";
+import GameBoard from "./Components/GameBoard.jsx";
+
+function App() {
+    return (
+        <main>
+            <div id="game-container">
+                <ol id = "players">
+                    <Player initialName="Player 1" symbol="X" />
+                    <Player initialName="Player 2" symbol="O" />
+                </ol>
+                <GameBoard />
+            </div>
+
+            Log
+        </main>
+    )
+}
+
+export default App
