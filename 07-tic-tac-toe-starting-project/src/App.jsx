0e73facbeb73b25ebeb3eb18375a49c065ef92de
Rohit Kumar Chintamani <rohitkumarchintamani@Mac.cable.rcn.com>
2025-08-21 19:27:18 -0500

    Removed unnecessary state update

--- 07-tic-tac-toe-starting-project/src/App.jsx
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -3,19 +3,29 @@ import GameBoard from "./Components/GameBoard.jsx";
 import Log from "./Components/Log.jsx";
 import {useState} from "react";
 
+function deriveActivePlayer(gameTurns) {
+
+    let currentPlayer = 'X';
+
+    if (gameTurns.length > 0 && gameTurns[0].player === 'X') {
+        currentPlayer = 'O';
+    }
+
+    return currentPlayer;
+
+}
+
 function App() {
 
-    const [activePlayer, setActivePlayer] = useState('X');
+    /*const [activePlayer, setActivePlayer] = useState('X');*/
     const [gameTurns, setGameTurns] = useState([]);
 
-    function handleSelectSquare(rowIndex, colIndex) {
-        setActivePlayer((curActivePlayer) => curActivePlayer === 'X' ? 'O' : 'X');
-        setGameTurns(prevTurns => {
-            let currentPlayer = 'X';
+    const activePlayer = deriveActivePlayer(gameTurns);
 
-            if (prevTurns.length > 0 && prevTurns[0].player === 'X') {
-                currentPlayer = 'O';
-            }
+    function handleSelectSquare(rowIndex, colIndex) {
+        /*setActivePlayer((curActivePlayer) => curActivePlayer === 'X' ? 'O' : 'X');*/
+        setGameTurns(prevTurns => {
+            const currentPlayer = deriveActivePlayer(prevTurns);
 
             const updatedTurns = [
                 {square: {row: rowIndex, col: colIndex}, player: currentPlayer},
