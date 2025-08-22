30b12bf1b31723c6f5a8009bc7c62d2ef6a71cab
Rohit Kumar Chintamani <rohitkumarchintamani@Mac.cable.rcn.com>
2025-08-21 21:31:02 -0500

    Made changes to app.jsx

--- 07-tic-tac-toe-starting-project/src/App.jsx
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -32,21 +32,27 @@ function App() {
     let gameBoard = initialGameBoard;
 
     for (const turn of gameTurns) {
-        const {square, player} = turn;
-        const {row, col} = square;
+        const { square, player } = turn;
+        const { row, col } = square;
+
         gameBoard[row][col] = player;
     }
 
     let winner;
 
     for (const combination of WINNING_COMBINATIONS) {
-        const firstSquareSymbol = gameBoard[combination[0].row][combination[0].col];
-        const secondSquareSymbol = gameBoard[combination[1].row][combination[1].col];
-        const thirdSquareSymbol = gameBoard[combination[2].row][combination[2].col];
+        const firstSquareSymbol =
+            gameBoard[combination[0].row][combination[0].column];
+        const secondSquareSymbol =
+            gameBoard[combination[1].row][combination[1].column];
+        const thirdSquareSymbol =
+            gameBoard[combination[2].row][combination[2].column];
 
-        if (firstSquareSymbol &&
+        if (
+            firstSquareSymbol &&
             firstSquareSymbol === secondSquareSymbol &&
-            firstSquareSymbol === thirdSquareSymbol) {
+            firstSquareSymbol === thirdSquareSymbol
+        ) {
             winner = firstSquareSymbol;
         }
     }
