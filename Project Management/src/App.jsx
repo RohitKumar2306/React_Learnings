1e0499a8ba27bd37a770fb318a084ce83b972cf5
Rohit Kumar Chintamani <rohitkumarchintamani@Mac-321.cable.rcn.com>
2025-08-30 13:38:53 -0500

    Added modal for input validation and made changes to add and view projects

--- Project Management/src/App.jsx
+++ Project Management/src/App.jsx
@@ -19,19 +19,43 @@ function App() {
         });
     }
 
+    function handleAddProject(projectData) {
+        setProjectsState(prevState => {
+            const newProject = {
+                ...projectData,
+                id: Math.random()
+            };
+
+            return {
+                ...prevState,
+                selectedProjectId: undefined,
+                projects: [...prevState.projects, newProject]
+            }
+        })
+    }
+
+    function handleCancel() {
+        setProjectsState(prevState => {
+            return {
+                ...prevState,
+                selectedProjectId: undefined,
+            }
+        })
+    }
+
     let content;
 
     if (projectsState.selectedProjectId === null) {
-        content = <NewProject />;
+        content = <NewProject onAddProject={handleAddProject} onCancel={handleCancel}/>;
     } else if (projectsState.selectedProjectId === undefined) {
         content = <NoProjectSelected onStartAddProject = {handleStartAddProject} />;
     }
 
     return (
-    <main className="h-screen my-8 flex gap-8">
-        <SideBar onStartAddProject = {handleStartAddProject} />
-        {content}
-    </main>
+        <main className="h-screen my-8 flex gap-8">
+            <SideBar onStartAddProject = {handleStartAddProject} projects={projectsState.projects} />
+            {content}
+        </main>
     );
     }
 
